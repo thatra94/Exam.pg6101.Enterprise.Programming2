8089392d2fe832fb16f81483755083047b052d30
Your Name <you@example.com>
2019-11-19 14:29:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Exam.pg6100.enterprise.programming2
+ Exam for pg6100 enterprise programming
---
+++ social-media-service/src/test/kotlin/com/exam/pg6100/socialmediaservice/SocialMediaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exam.pg6100.socialmediaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialMediaServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
